a170ca385c2ca53ec28a78d26c4c55c5d8a73b98
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-01-23 22:42:49 +0900

    add: dart simple package追加、description, README.md編集

---
+++ lib/PlotDart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library PlotDart;
+
+export 'src/PlotDart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
